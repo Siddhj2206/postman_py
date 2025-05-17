3aee6ba7aecf48178088a01c38acf14aac20ee2b
Siddhj2206 <siddhj2206@gmail.com>
2025-05-17 13:11:33 +0530

    Moved argument parsing to a seperate function

--- postman_py.py
+++ postman_py.py
@@ -4,34 +4,43 @@ import sys
 def help():
     print("Usage: postman_py.py <METHOD> <URL> -H 'key:value' -d DATA")
 
+def parse_args(args):
+    if len(args) < 2:       # Print help if no URL porvided
+        help()
+        sys.exit(1)
+
+    # Correct number of args passed
+    r = {'method': args[0].upper(),
+         'url': args[1],
+         'header': {},
+         'data': None}
+
+    i = 2
+    while i < len(args):
+        if args[i] == '-H' and i + 1 < len(args):
+            h = args[i+1]
+            key, val = h.split(":", 1)
+            r['header'][key.strip()] = val.strip()
+            i += 2
+        elif args[i] == 'd' and i + 1 < len(args):
+            r['data'] = args[i+1]
+            i += 2
+        else:
+            i += 1
+
+    return r
+
+
 args = sys.argv[1:]
-if len(args) < 2:       # Print help if no URL porvided
-    help()
-    sys.exit(1)
-
-# Correct number of args passed
-method = args[0].upper()
-url = args[1]
-header = {}
-data = None
-
-i = 2
-while i < len(args):
-    if args[i] == '-H' and i + 1 < len(args):
-        h = args[i+1]
-        key, val = h.split(":", 1)
-        header[key.strip()] = val.strip()
-        i += 2
-    elif args[i] == 'd' and i + 1 < len(args):
-        data = args[i+1]
-        i += 2
-    else:
-        i += 1
+req = parse_args(args)
 
 # print(args, method, url, header, data)
-
 try:
-    response = requests.request(method, url, headers=header, data=data)
+    response = requests.request(req['method'],
+                                req['url'],
+                                headers=req['header'],
+                                data=req['data'])
+
 except requests.RequestException as err:
     print("Error:", err)
     sys.exit(1)
